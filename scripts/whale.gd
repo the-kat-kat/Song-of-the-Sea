a9c2b7cf4933b5e11651e498042853101e83644b
katiikati <166103545+katiikati@users.noreply.github.com>
2025-10-03 02:10:42 -0700

    Added 2nd whale

--- scripts/whale.gd
+++ scripts/whale.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var dialogue_resource: DialogueResource
 @export var interact_icon: Sprite2D
 @onready var player = get_tree().get_nodes_in_group("player")[0]
-
+@onready var actionable = $WhaleActionable
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interact_icon.visible = false
+	actionable.dialogue_resource = dialogue_resource
 
 
 func _on_whale_actionable_body_entered(body: Node2D) -> void:
