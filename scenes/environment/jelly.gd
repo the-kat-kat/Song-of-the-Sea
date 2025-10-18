ed56c58a2143c0d0951d71ddd1dc3dff8db3f134
katiikati <166103545+katiikati@users.noreply.github.com>
2025-10-18 13:01:26 -0700

    Random jelly pop up

--- scenes/environment/jelly.gd
+++ scenes/environment/jelly.gd
@@ -4,14 +4,18 @@ var wait_time = 3.0
 
 func _ready() -> void:
 	randomize()
+	wait_time = randf_range(1.0, 5.0)
 	random_move()
 
 func random_move() -> void:
-	await get_tree().create_timer(wait_time).timeout
-	wait_time = randf()*5
-	position = Vector2(randf_range(-1500,1500),randf_range(-800,800))
-	var scaler = randf_range(0.5, 2)
-	scale = Vector2(scaler, scaler)
+	randomize()
 	play()
 	await animation_finished
+	
+	wait_time = randf_range(1.0, 5.0)
+	position = Vector2(randf_range(-1500,1500),randf_range(-800,800))
+	var scaler = randf_range(0.1, 0.7)
+	scale = Vector2(scaler, scaler)
+	
+	await get_tree().create_timer(wait_time).timeout
 	random_move()
